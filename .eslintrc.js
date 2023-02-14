5b0de14c9c5d0a4319184f82ea1a589596260cae
Alexander <73581770+SashaLeshiy@users.noreply.github.com>
2023-02-14 22:07:27 +0300

    установка и настройка stylelint + плагин i18next

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'i18next',
   ],
   rules: {
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
@@ -45,6 +46,7 @@ module.exports = {
     'no-shadow': 'off',
     'no-unused-vars': 'warn',
     'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,
