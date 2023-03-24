6d114d8a3f4ed2f180d7f40952b2f4e16dc129b3
Alexander <73581770+SashaLeshiy@users.noreply.github.com>
2023-03-24 10:37:31 +0300

    update app router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,15 @@ import { Suspense, useEffect } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { NavBar } from 'widgets/nav-bar';
 import { Sidebar } from 'widgets/Sidebar';
-import { userActions } from 'entities/User';
-import { useDispatch } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTheme } from './providers/ThemeProvider/ui';
 import { AppRouter } from './providers/router';
 
 const App = () => {
   const { theme } = useTheme();
   const dispatch = useDispatch();
+  const inited = useSelector(getUserInited);
 
   useEffect(() => {
     dispatch(userActions.initAuthData());
@@ -21,7 +22,7 @@ const App = () => {
         <NavBar />
         <div className="content-page">
           <Sidebar />
-          <AppRouter />
+          {inited && <AppRouter />}
         </div>
       </Suspense>
     </div>
